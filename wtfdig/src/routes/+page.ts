f80965d3d102278c53c6aeafbcc7ead54e25f72c
NingAnAbu <shuutinggyuan@outlook.com>
2025-04-21 23:30:56 +0800

    修改了C队的两处高亮错误

--- wtfdig/src/routes/+page.ts
+++ wtfdig/src/routes/+page.ts
@@ -9346,7 +9346,7 @@ const ziyanStrat: Strat = {
                 },
             ],
             swapNote: "我换到了西侧的平台上！现在怎么站?",
-            swapWarning:"请迅速挑衅重新建立仇恨，T需要拉BOSS朝场外，",
+            swapWarning:"请迅速挑衅重新建立仇恨，T需要拉BOSS朝场外",
             swapStrats: [
                 {
                     mechanic: '踩塔',
@@ -9394,7 +9394,7 @@ const ziyanStrat: Strat = {
                     mechanic: '潜地炮预站位',
                     description: '',
                     imageUrl: './strats/ziyan/tiles-northwest-chaser-pre.png',
-                    mask: 'radial-gradient(circle at 55.1% 86.1%, black 12%, rgba(0, 0, 0, 0.2) 12%)'
+                    mask: 'radial-gradient(circle at 78.6% 80%, black 12%, rgba(0, 0, 0, 0.2) 12%)'
                 },
                 {
                     mechanic: '凝缩式波动炮',
@@ -10365,7 +10365,7 @@ const ziyanStrat: Strat = {
                     mechanic: '潜地炮预站位',
                     description: '',
                     imageUrl: './strats/ziyan/tiles-northwest-chaser-pre.png',
-                    mask: 'radial-gradient(circle at 55.1% 86.1%, black 12%, rgba(0, 0, 0, 0.2) 12%)'
+                    mask: 'radial-gradient(circle at 78.6% 80%, black 12%, rgba(0, 0, 0, 0.2) 12%)'
                 },
                 {
                     mechanic: '凝缩式波动炮',
@@ -10507,7 +10507,7 @@ const ziyanStrat: Strat = {
                     mechanic: '潜地炮预站位',
                     description: '',
                     imageUrl: './strats/ziyan/tiles-northwest-chaser-pre.png',
-                    mask: 'radial-gradient(circle at 55.1% 86.1%, black 12%, rgba(0, 0, 0, 0.2) 12%)'
+                    mask: 'radial-gradient(circle at 78.6% 80%, black 12%, rgba(0, 0, 0, 0.2) 12%)'
                 },
                 {
                     mechanic: '凝缩式波动炮',
